eddadbfdd3831f1334621018a4f8e5010203dd32
A-Siam <abdullah.khaled.kamal0@gmail.com>
2021-08-08 20:14:24 +0200

    feat: add comment editing test

--- Bloggie-Backend/src/tests/integration/comments/api/data-api.test.ts
+++ Bloggie-Backend/src/tests/integration/comments/api/data-api.test.ts
@@ -2,6 +2,10 @@ import { login } from "@tests/integration/utils/login";
 import setupTeardownGraphQL_API from "@tests/utils/api/setup-teardown";
 import articleCreation from "@tests/utils/articles/article-creation";
 import axios from "axios";
+import CommentsLogic from "@controllers/data-interaction/comment/comments-logic";
+import CommentsLogicImpl from "@controllers/data-interaction/comment/comments-logic-impl";
+import { commentDependencyValidator } from "@controllers/data-interaction/comment/dependency-validator";
+import { Comment } from "@models/article/comments";
 
 describe("Comments data API test suite", () => {
   const PORT = setupTeardownGraphQL_API();
@@ -35,4 +39,52 @@ describe("Comments data API test suite", () => {
     expect(res.content).toEqual(commentContent);
     expect(res.date).toBeTruthy();
   });
+
+  test("should edit comment successfully", async () => {
+    const { article, user } = await articleCreation();
+    const commentLogic: CommentsLogic = new CommentsLogicImpl();
+    const content = "the best thing in this world!";
+    const tempComment = new Comment();
+    tempComment.content = content;
+    const comment = await commentLogic.addComment(
+      article._id,
+      user._id,
+      tempComment,
+      commentDependencyValidator
+    );
+    const commentContent = "yoyoyoyoyoyoyoyoyo";
+    const accessToken = await login(graphQLUrl, user.email, user.password!);
+    const res = await axios
+      .post(
+        graphQLUrl,
+        {
+          query: `
+				mutation commentEdit {
+					editComment(content: "${commentContent}", commentId: "${comment._id}") {
+						date,
+						content,
+						commentId
+					}
+				}
+			`,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      )
+      .then((res) => res.data)
+      .then((content) => content.data.editComment);
+    expect(res.date).not.toEqual(comment.date);
+    expect(res.content).toEqual(commentContent);
+		// to fix serialize to the same string I've used JSON.stringify to serialize
+    expect(JSON.stringify(res.commentId)).toEqual(
+      JSON.stringify(comment.commentId)
+    );
+  });
+	test.todo('should reject updating comment with by different author', async () => {
+		
+	})
+	
 });
