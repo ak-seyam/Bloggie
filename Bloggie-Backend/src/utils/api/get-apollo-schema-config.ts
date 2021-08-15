77248f3ce9c71ee2af9fd00ef852bef5f01e1c02
A-Siam <abdullah.khaled.kamal0@gmail.com>
2021-08-16 01:42:31 +0200

    remove uesless import from apollo config

--- Bloggie-Backend/src/utils/api/get-apollo-schema-config.ts
+++ Bloggie-Backend/src/utils/api/get-apollo-schema-config.ts
@@ -1,9 +1,5 @@
 import { ApolloServerExpressConfig } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
-import UserResolver from "@controllers/resolvers/user-resolver";
-import ArticleResolver from "@controllers/resolvers/aricle-resolver";
-import CommentsResolver from "@controllers/resolvers/comments-resolver";
-import PingResolver from "@controllers/resolvers/ping";
 
 export default async function getApolloConfig(): Promise<ApolloServerExpressConfig> {
   const resolversDir = `${__dirname}/../../controllers/resolvers/*.ts`;
